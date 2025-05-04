e00d9de3fa65d32507e6a7017dd3cb3f4e34d560
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-05-04 21:03:09 +0200

    refactored (from device-kostal)

---
+++ io.openems.edge.kostal.plenticore/test/io/openems/edge/kostal/plenticore/ess/KostalManagedEssImplTest.java
@@ -0,0 +1,29 @@
+package io.openems.edge.kostal.plenticore.ess;
+
+import org.junit.Test;
+
+import io.openems.edge.bridge.modbus.test.DummyModbusBridge;
+import io.openems.edge.common.test.ComponentTest;
+import io.openems.edge.common.test.DummyConfigurationAdmin;
+
+public class KostalManagedEssImplTest {
+
+	  @Test
+	  public void test() throws Exception {
+	    new ComponentTest(new KostalManagedEssImpl()) //
+	      .addReference("cm", new DummyConfigurationAdmin()) //
+	      .addReference("setModbus", new DummyModbusBridge("modbus0")) //
+	      .activate(
+	        MyConfig.create() //
+	          .setId("ess0") //
+	          .setReadOnlyMode(true) //
+	          .setModbusId("modbus0") //
+	          .setCapacity(10000) //
+	          .setWatchdog(20)
+	          .setTolerance(30)
+	          .setModbusUnitId(71) //
+	          .setDebugMode(true)
+	          .build()
+	      ); //
+	  }
+}
